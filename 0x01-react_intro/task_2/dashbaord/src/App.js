4e3e092fa9f32cb7b4d515657fdb30ea9c784547
EnduranceApeh <apehendurance2@gmail.com>
2024-07-25 02:48:48 +0100

    commit chnage

--- 0x01-react_intro/task_2/dashbaord/src/App.js
+++ 0x01-react_intro/task_2/dashbaord/src/App.js
@@ -1,28 +1,26 @@
-import logo from './holberton-logo.jpg';
-import './App.css'
-import {getFullYear, getFooterCopy} from './utils'
+import logo from "./holberton-logo.jpg";
+import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="Holberton logo" />
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-      </header>
-
+      </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
-        <form>
+	 <form>
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email"></input>
+          <input type="email" name="email"></input>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password"></input>
+          <input type="password" name="password"></input>
           <button>OK</button>
         </form>
       </div>
-
       <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+        Copyright {getFullYear()} - {getFooterCopy()}
       </div>
     </div>
   );
